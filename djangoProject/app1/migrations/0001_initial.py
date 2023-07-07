80d44c20642965f0d505fb697182e19c536d479f
JiahuiChenStudent <JiahuiChenStudent>
2023-07-07 20:20:23 +0800

    update django project

---
+++ djangoProject/app1/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+from django.db import migrations, models
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='WriteSomething',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=100)),
+                ('content', models.TextField()),
+                ('image', models.ImageField(blank=True, null=True, upload_to='images/', validators=[django.core.validators.FileExtensionValidator(['jpg', 'jpeg', 'png'])])),
+            ],
+        ),
+    ]
