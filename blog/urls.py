9a6825beb3b523d3e09c73954766e8b43623bfd8
ayeshasani12 <136660171+ayeshasani12@users.noreply.github.com>
2023-07-21 11:07:59 +0530

    Adding Blog Post through Admin Panel
    And a detailed page for the Blog section

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    # path('', views.main, name='main'),
+     path('', views.main, name='main'),
     # Add other URL patterns as needed
+    path('detail/<int:blog_id>/',views.detail,name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Blog
-
-
-
 
 def main(request):
     blog = Blog.objects.all()
     return render(request, 'home/main.html', {'blog': blog})
 
-# Create your views here.
-
-# blog/views.py
-
-
-
-# def detail(request, blog_id):
-#     blog_post = get_object_or_404(Blog, pk=blog_id)
-#     return render(request, 'blog/detail.html', {'blog_post': blog_post})
+def detail(request, blog_id):
+    blog_post = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog_post': blog_post})
 
