aeaae2b325bdbd944b83067eb99355e68cee999d
JiahuiChenStudent <136550017+JiahuiChenStudent@users.noreply.github.com>
2023-07-21 19:40:53 -0700

    Delete djangoProject/app1 directory

--- djangoProject/app1/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from app1.models import WriteSomething
-
-class WriteSomethingForm(forms.ModelForm):
-    class Meta:
-        model = WriteSomething
-        fields = ['title', 'content']
