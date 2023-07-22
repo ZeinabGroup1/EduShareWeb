5886d5f32a2d6bd4439f947cc2a3f2fa339ed0b4
Zeinab <136505866+ZeinabGroup1@users.noreply.github.com>
2023-07-21 18:18:52 -0700

    Dashboard, Favorite, profile, register pages

---
+++ account/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import *
+
+
+class UserRegisterForm(forms.Form):
+    user_name = forms.CharField(max_length=100, )
+    email = forms.EmailField()
+    password = forms.CharField(max_length=20)
+
+    def clean_user_name(self):
+        user = self.cleaned_data['user_name']
+        if User.objects.filter(username=user).exists():
+            raise forms.ValidationError('User Exists')
+        return user
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('email already exist')
+        return email
+
+
+class UserLoginForm(forms.Form):
+    user = forms.CharField()
+    password = forms.CharField()
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('phone', 'first_name', 'last_name', 'country', 'city', 'address', 'image',)
